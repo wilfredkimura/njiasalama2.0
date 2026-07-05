7eac498d130e1c6ef7854df9d2b36c5babb46083
wilfredkimura <wilfredmutahi9@gmail.com>
2026-07-05 21:17:11 +0300

    build(android): bump versionCode to 2 and versionName to 1.0.1-alpha.1

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "com.njiasalama"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.1-alpha.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
